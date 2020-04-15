10f35ce986b5d4edbd32770e00d15512f2adb7b5
Shona Lowden <lowdensh@gmail.com>
2020-04-15 23:14:31 +0100

    basic logic for getting user date

--- whensplode.py
+++ whensplode.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-# do chmod 744 FILENAME.extension
-# run with "./FILENAME.extension"
-# or just "py whensplode.py"
 
 import datetime
+import sys
 
 
 def datef(date):
@@ -21,5 +18,23 @@ def announce_days_until(date_target, message):
           f"{message}")
 
 
+def get_user_date():
+    year = int(input(
+        "Year (YYYY): "))
+    month = int(input(
+        " Month (MM): "))
+    day = int(input(
+        "   Day (DD): "))
+    user_date = datetime.datetime(year, month, day)
+    announce_days_until(user_date, None)
+
+
 date_shona_30 = datetime.datetime(2026, 11, 12)  # Shona's 30th birthday
 announce_days_until(date_shona_30, "Shona's 30th birthday, when she will explode.")
+
+while True:
+    response = input("Check days until a different date? (y/n): ")
+    if response == "y":
+        get_user_date()
+    elif response == "n":
+        sys.exit(0)
