2457400afa33dc2a95905621bfb6f500460573da
Shona Lowden <lowdensh@gmail.com>
2020-04-19 00:09:16 +0100

    allow user dates in the past

--- whensplode.py
+++ whensplode.py
@@ -1,6 +1,7 @@
 
 import datetime
 import sys
+from calendar import monthrange
 
 
 def datef(date):
@@ -16,7 +17,7 @@ def announce_days_until(date_target, message):
     days = days_until(date_target)
 
     if days < -1:
-        announcement = f"{datef(date_target)} was {days} days ago"
+        announcement = f"{datef(date_target)} was {abs(days)} days ago"
     elif days == -1:
         announcement = f"{datef(date_target)} was yesterday"
     elif days == 0:
@@ -26,7 +27,6 @@ def announce_days_until(date_target, message):
     elif days > 1:
         announcement = f"There are {days} days until {datef(date_target)}"
 
-    # TODO function to select random message
     if message:
         announcement += f", {message}"
     else:
@@ -35,57 +35,28 @@ def announce_days_until(date_target, message):
     print(announcement)
 
 
-def int_input(prompt):
+def int_input(prompt, min, max):
     try:
-        return int(input(prompt))
+        user_input = int(input(prompt))
+        if user_input < min:
+            print(f"Error: Input below minimum value. Enter a number {min} or higher.")
+            return int_input(prompt)
+        if user_input > max:
+            print(f"Error: Input above maximum value. Enter a number {max} or lower.")
+            return int_input(prompt)
+        return user_input
     except:
         print("Error: Expected an integer. Enter a valid number.")
         return int_input(prompt)
 
 
-def get_date_part(part, future):
-    date_current = datetime.datetime.now()
-
-    if part == "year":
-        date_part = int_input("Year (YYYY): ")
-        if future:  # TODO refactor into future date part function
-            while date_part < date_current.year:
-                print(f"Error: Date must be in the future. Enter {date_current.year} or later.")
-                date_part = int_input("Year (YYYY): ")
-
-    elif part == "month":
-        date_part = int_input(" Month (MM): ")
-        # TODO valid month check
-        if future:
-            while date_part < date_current.month:
-                print(f"Error: Date must be in the future. Enter {date_current.month} or later.")
-                date_part = int_input(" Month (MM): ")
-
-    elif part == "day":
-        date_part = int_input("   Day (DD): ")
-        # TODO valid day check
-        if future:
-            while date_part < date_current.day:
-                print(f"Error: Date must be in the future. Enter {date_current.day} or later.")
-                date_part = int_input("   Day (DD): ")
-
-    return date_part
-
-
-def get_user_date():  # TODO allow user dates in the past
-    date_current = datetime.datetime.now()
-
-    year = get_date_part("year", True)
-
-    if year == date_current.year:
-        month = get_date_part("month", True)
-    else:
-        month = get_date_part("month", False)
-
-    if (year == date_current.year) and (month == date_current.month):
-        day = get_date_part("day", True)
-    else:
-        day = get_date_part("day", False)
+def get_user_date():
+    year = int_input(
+        "Year (YYYY): ", 1970, 3000)
+    month = int_input(
+        " Month (MM): ", 1, 12)
+    day = int_input(
+        "   Day (DD): ", 1, monthrange(year, month)[1])
 
     return datetime.datetime(year, month, day)
 
@@ -97,8 +68,9 @@ date_shona_30 = datetime.datetime(2026, 11, 12)  # Shona's 30th birthday
 announce_days_until(date_shona_30, "Shona's 30th birthday, when she will explode.")
 
 while True:
-    response = input("Check days until a different future date? (y/n): ")
+    response = input("Check days until a different date? (y/n): ")
     if response == "y":
+        # TODO function to select random message for announcements
         announce_days_until(get_user_date(), None)
     elif response == "n":
         sys.exit(0)
